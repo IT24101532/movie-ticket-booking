7c4772ec018aa71fd8cb53f7dd173a8708a33cf7
IT24101469 <it24101469@my.sliit.lk>
2025-05-19 15:39:28 +0530

    it24101469 addding new User.java

---
+++ src/main/java/com/movieticket/movie_ticket_booking/model/User.java
@@ -0,0 +1,86 @@
+package com.movieticket.movie_ticket_booking.model;
+
+public class User  {
+    private String id;
+    private String firstName, lastName, email, gender, nic, address, mobile, password;
+    private String role; // Added role field for user/admin distinction
+
+    public User() {
+        this.role = "USER"; // Default role
+    }
+
+    public User(String firstName, String lastName, String email, String gender,
+                String nic, String address, String mobile, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.nic = nic;
+        this.address = address;
+        this.mobile = mobile;
+        this.password = password;
+        this.role = "USER";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getNic() {
+        return nic;
+    }
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getMobile() {
+        return mobile;
+    }
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
